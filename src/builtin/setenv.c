6e96b3bf7330afcdd8f7a2403c6872ee406b6d25
matheo.lucak <matheo.lucak@epitech.eu>
2020-01-29 16:54:16 +0100

    [FIX] Regular setenv

--- src/builtin/setenv.c
+++ src/builtin/setenv.c
@@ -15,62 +15,46 @@ int set_env_error(char **input, char **env)
 {
     if (!input || !env)
         return (84);
-    if (my_arrlen(input) % 2 == 0) {
-        my_printf("%ssetenv: Too many arguments.\n", input[0]);
+    if (my_arrlen(input) > 3) {
+        my_printf("%s: Too many arguments.\n", input[0]);
         return (84);
     }
     return (0);
 }
 
-char *new_env_var(char **input, int i)
+char *new_env_var(char **input)
 {
     char *tmp1 = NULL;
     char *tmp2 = NULL;
 
-    if (!input)
+    if (!input || !input[0] || !input[1])
         return (NULL);
-    if (!input[i] || !input[i + 1])
-        return (NULL);
-    tmp1 = my_strcat(input[i], "=");
+    tmp1 = my_strcat(input[1], "=");
     if (!tmp1)
         return (NULL);
-    tmp2 = my_strcat(tmp1, input[i + 1]);
+    if (my_arrlen(input) == 2)
+        return (tmp1);
+    tmp2 = my_strcat(tmp1, input[2]);
     free(tmp1);
     return (tmp2);
-}
-
-void set_newenv_var(char **input, char **new_env, char **env, int i)
-{
-    int j = 0;
-    int shift_j = 0;
-    int var_nb = (my_arrlen(input) - 1) / 2;
-    int env_idx = 0;
-
-    while (j < var_nb) {
-        env_idx = find_env(env, input[j * 2 + 1]);
-        if (env_idx != -1) {
-            new_env[env_idx] = new_env_var(input, j * 2 + 1);
-            shift_j--;
-        } else
-            new_env[i + j + shift_j] = new_env_var(input, j * 2 + 1);
-        j++;
-    }
-    new_env[i + j + shift_j] = NULL;
 }
 
 char **dup_set_env(char **input, char **env)
 {
     int i = 0;
-    char **new_env = malloc(sizeof(char *) * (my_arrlen(env) +
-        (my_arrlen(input) - 1) / 2 + 1));
+    char **new_env = malloc(sizeof(char *) * (my_arrlen(env) + 2));
 
-    if (!new_env)
+    if (!new_env || !env)
         return (NULL);
     while (env[i]) {
         new_env[i] = my_strdup(env[i]);
         i++;
     }
-    set_newenv_var(input, new_env, env, i);
+    if (find_env(env, input[1]) == -1)
+        new_env[i] = new_env_var(input);
+    else
+        new_env[find_env(env, input[1])] = new_env_var(input);
+    new_env[i + 1] = NULL;
     free_env(env);
     return (new_env);
 }
@@ -79,6 +63,10 @@ int set_env(char **input, char ***env)
 {
     if (set_env_error(input, *env) == 84)
         return (84);
+    if (my_arrlen(input) == 1) {
+        my_show_word_array(*env);
+        return (1);
+    }
     *env = dup_set_env(input, *env);
     return (1);
 }
